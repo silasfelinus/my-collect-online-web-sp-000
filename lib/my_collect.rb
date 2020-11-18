6b1be2ee5034e8cd162f56e8e20fbee384157be7
Silas Michael Knight <silasfelinus@gmail.com>
2020-11-18 01:59:47 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,16 @@
 def my_collect(array)
   i = 0
+<<<<<<< HEAD
   revised_array = []
   while i < array.length
     revised_array << yield(array[i])
     i += 1
   end
   revised_array
+=======
+  while i < array.length
+    yield array[i]
+    i += 1
+  end
+>>>>>>> dda2aa9ed64cf54398a71af6f91a5e02984cd8ab
 end
